6055573f515c41eae4083fceda4441fdce280fb8
Kole Chaicharee <108165007+kole-wc@users.noreply.github.com>
2026-06-16 14:41:11 +0900

    finished part1c passing state

--- learn/part1/src/App.jsx
+++ learn/part1/src/App.jsx
@@ -1,26 +1,33 @@
-const Hello = ({ name, age }) => {
-const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
 
+const Display = (props) => {
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
-    <>
-      <p>Greetings</p>
-      <Hello name='Maya' age={26 + 10}/>
-      <Hello name={name} age={age}/>
-    </>
+    <div>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text={'plus'}/>
+      <Button onClick={decreaseByOne} text={'minus'}/>
+      <Button onClick={setToZero} text={'zero'}/>
+    </div>
   )
 }
 
