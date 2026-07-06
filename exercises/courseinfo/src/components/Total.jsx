08cf165a153269af8471e7d066b6e443a39d7aa5
Kole Chaicharee <108165007+kole-wc@users.noreply.github.com>
2026-07-06 14:20:21 +0900

    removed the old courseinfo project

--- exercises/courseinfo/src/components/Total.jsx
+++
@@ -1,8 +0,0 @@
-const Total = ({ parts }) => {
-    return (
-        <b>total of {parts.reduce((totalExercises, part) => totalExercises + part.exercises, 0)} exercises</b>
-    )
-}
-
-
-export default Total
